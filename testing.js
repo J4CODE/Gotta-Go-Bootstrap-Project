69db7e99e4c392d80216954d450151476fd6e4b2
R Jay <15009097+J4CODE@users.noreply.github.com>
2020-12-04 10:53:48 -0500

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,1 +1,15 @@
-JS Code Testing
+// JS Code Testing
+$(document).ready(function () {
+    // executes when HTML-Document is loaded and DOM is ready
+    console.log("document is ready");
+  
+    $('[data-toggle="offcanvas"], #navToggle').on('click', function () {
+      $('.offcanvas-collapse').toggleClass('open')
+    })
+  });
+  
+  window.onload = function () {
+    // executes when complete page is fully loaded, including all frames, objects and images
+    console.log("window is loaded");
+  
+  };  
